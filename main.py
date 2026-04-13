5d3f6fe3f0a0c7cb9a7b17dd48ba7448d51dceff
Rameez Sarwar <vetrameez@gmail.com>
2026-04-13 19:38:57 +0500

    New Appointment Type Fertility Added with All U.S 50 min removed completely

--- main.py
+++ main.py
@@ -781,11 +781,7 @@ def _is_psych_eval_eligible(apt_type: dict) -> bool:
     cat = apt_type.get("category", "").upper()
     if PSYCH_EVAL_CATEGORY_KEYWORD not in cat:
         return False
-    try:
-        if int(apt_type.get("duration", 0)) != PSYCH_EVAL_ALLOWED_DURATION:
-            return False
-    except (TypeError, ValueError):
-        return False
+    
     if not resolve_calendar_ids(apt_type):
         return False
     if _is_test_type(apt_type):
